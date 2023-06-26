fa4f06cb5c7a059e6fe935b6d85d57dbd739389b
gabriele-cmd <75305819+gabriele-cmd@users.noreply.github.com>
2023-06-26 21:58:26 +0200

    Created Activity class header and cpp file.

--- Activity.h
+++ Activity.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef REGISTRO_ATTIVITÀ_ACTIVITY_H
 #define REGISTRO_ATTIVITÀ_ACTIVITY_H
 
@@ -31,40 +30,4 @@ private:
     QTime startTime;
     QTime endTime;
 };
-
-=======
-#ifndef REGISTRO_ATTIVITÀ_ACTIVITY_H
-#define REGISTRO_ATTIVITÀ_ACTIVITY_H
-
-#include <QTime>
-#include <QString>
-
-class Activity {
-public:
-    //Constructor
-    Activity(QString desc, Qtime st, Qtime et);
-    //Destructor
-    ~Activity() = default;
-
-    //Getters (const to disallow modifications)
-    QString getDescription() const;
-    QTime getStartTime() const;
-    QTime getEndTime() const;
-
-    //Setters
-    void setDescription(QString desc);
-    void setStartTime(QTime start);
-    void setEndTime(QTime end);
-
-    //Overloading operators
-    bool operator==(const Activity& right) const;
-    bool operator!=(const Activity& right) const;
-
-private:
-    QString description;
-    QTime startTime;
-    QTime endTime;
-};
-
->>>>>>> 5e6d8999186e3323e23682787b75f83db0404946
 #endif //REGISTRO_ATTIVITÀ_ACTIVITY_H
